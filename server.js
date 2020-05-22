673593ae5fcbef5adc403de7618d6685cf0b4f48
Dan Lyons <danielclyons@yahoo.com>
2020-05-22 16:47:27 -0700

    html pages working, populate notes

---
+++ server.js
@@ -0,0 +1,74 @@
+const fs = require('fs');
+const path = require('path');
+const express = require('express');
+
+const { notes } = require('./db/notes');
+
+const PORT = process.env.PORT || 3004;
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
+// app.get("/", (req, res) => {
+//   res.sendFile(path.join(__dirname, "./public/assets/css/style.css"));
+// });
+
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/notes.html"));
+});
+
+app.get("/api/notes", (req, res) => {
+  console.log(notes);
+  res.json(notes);
+})
+
+app.post("/api/notes", (req, res) => {
+  console.log(req.body);
+  let length = notes.length;
+  console.log(length);
+  let newId = length;
+  newId = toString(newId);
+  // notes.push(req.body);
+  let newArray = notes;
+  console.log(newArray);
+  newArray.newArray.push("id:" + newId);
+  console.log(newArray);
+
+  // newArray.push(newId);
+  // console.log(newArray);
+
+  // res("test");
+})
+
+// app.get('/api/test', (req, res) => {
+
+//   let results = db;
+//   console.log(" console : ");
+//   // console.log(req);
+//   console.log(results);
+//   res.json(results);
+//   // res.json(result);
+  
+// });
+
+// app.post('/api/db/db', (req, res) => {
+//   // set id based on what the next index of the array will be
+//   req.body.id = db.length.toString();
+
+//   // if any data in req.body is incorrect, send 400 error back
+//   if (!validateNote(req.body)) {
+//     res.status(400).send('The note is not properly formatted.');
+//   } else {
+//     const note = createNewNote(req.body, note);
+//     res.json(note);
+//   }
+// });
+
+app.listen(PORT, () => {
+  console.log(`API server now on port ${PORT}!`);
+});
